82914962a10f8b0356d49ff272caef28c7b6e641
Talesdev1 <talesdeveloper1@gmail.com>
2025-07-19 22:36:39 +0000

    add a more robust ssl configuration method

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,7 +3,7 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
+
   expect(responseBody.updated_at).toBeDefined();
   expect(responseBody.dependencies.database.version).toBeDefined();
   expect(responseBody.dependencies.database.max_connections).toBeDefined();
