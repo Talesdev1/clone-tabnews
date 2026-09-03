ef8b48ba001b1d1e8c339a0be0d85585ee022903
Talesdev1 <talesdeveloper1@gmail.com>
2026-09-03 10:19:08 -0300

    fix `prettier` style linting

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,4 @@
-import retry from "async-retry"
+import retry from "async-retry";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -19,5 +19,5 @@ async function waitForAllServices() {
 }
 
 export default {
-  waitForAllServices
-}
+  waitForAllServices,
+};
