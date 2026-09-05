0a4cbf7be719c6841e59359cac2f57444f6a8080
Talesdev1 <talesdeveloper1@gmail.com>
2026-09-04 21:29:12 -0300

    gix `eslint` and `prettier` linting

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -3,7 +3,7 @@ const { exec } = require("node:child_process");
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handdleReturn);
 
-  function handdleReturn(error, stdout, stderr) {
+  function handdleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
       process.stdout.write(".");
       checkPostgres();
